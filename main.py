37931c7796da08fd05c02d85eb11cde8ca591748
Sanasharon <144417993+Sanasharon@users.noreply.github.com>
2023-09-14 15:07:01 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,11 @@
-class BankAccount:
-    # create the constuctor with parameters: accountNumber, name and balance 
-    def __init__(self,accountNumber, name, balance):
-        self.accountNumber = accountNumber
-        self.name = name
-        self.balance = balance
-        
-    # create Deposit() method
-    def Deposit(self , d ):
-        self.balance = self.balance + d
-    
-    # create Withdrawal method
-    def Withdrawal(self , w):
-        if(self.balance < w):
-            print("impossible operation! Insufficient balance !")
-        else:
-            self.balance = self.balance - w
-    # create bankFees() method
-    def bankFees(self):
-        self.balance = (95/100)*self.balance
-        
-    # create display() method
-    def display(self):
-        print("Account Number : " , self.accountNumber)
-        print("Account Name : " , self.name)
-        print("Account Balance : " , self.balance , " $")
-        
-# Testing the code :
-newAccount = BankAccount(2178514584, "Albert" , 2700)
-# Creating Withdrawal Test
-newAccount.Withdrawal(300)
-# Create deposit test
-newAccount.Deposit(200)
-# Display account informations
-newAccount.display()
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
+
+
+number = 2
+res = fact_rec(number)
+
+print("The factorial of {} is {}".format(number, res))
